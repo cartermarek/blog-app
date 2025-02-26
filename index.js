49f78969c40c6ab6ba43469aeba8d5e07c5f9aec
Marek Carter <cartermarek@gmail.com>
2025-02-25 23:34:03 -0500

    initial commit blog-app

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const path = require('path')
 
 const app = new express()
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.SendFile(path.resolve(__direname, 'pages/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.SendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/post', (req, res) => {
+    res.SendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.SendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
 
 app.listen(4000, ()=>{
     console.log("App is listening on port 4000")
